6a198ea19f9bd4ec9d4f13294976b6fd2a87eb3e
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2024-01-08 19:56:47 +0545

    Create User added api route {username, email, password} simple

---
+++ backend/dbConnect.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/blogapp";
+
+// const connectToMongo = () => {
+//   mongoose.connect(mongoURI, () => {
+//     console.log("Connected to Mongo Successfully");
+//   });
+// };
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI);
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB Successfully");
+  });
+};
+
+// const connectToMongo = () => {
+//   if (mongoose.connect(mongoURI)) {
+//     console.log("Connected to MongoDB");
+//   } else {
+//     console.log("Unable Connect to MongoDB");
+//   }
+// };
+
+// connectToMongo().catch((err) => console.log(err));
+
+// async function connectToMongo() {
+//   await mongoose.connect(mongoURI);
+//   console.log("Connected to MongoDB Successfully");
+
+//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+// }
+
+module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
+const connectToMongo = require("./dbConnect");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
-const PORT = 5000;
+connectToMongo();
+const port = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.use(express.json());
+
+// Available routes
+app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/notes", require("./routes/notes"));
+
+app.listen(port, () => {
+  console.log(`Blogs Server is listening on http://localhost:${port}`);
 });
