80afa10cd8dec373bd08a3159493bb616d2bc785
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2024-01-09 12:25:55 +0545

    login end point with jwt and bycriptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
-
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SERECT = process.env.JWT_SERECT;
 
 // ROUTE 1 : Create a User using: POST "/api/auth/createuser/". Doesn't require Auth
 router.post(
@@ -31,20 +35,69 @@ router.post(
         }
       }
 
+      const salt = await bcrypt.genSalt(10);
+
+      var hashPass = await bcrypt.hash(req.body.password, salt);
+
       user = new User({
         username,
         email,
-        password,
+        password: hashPass,
       });
 
       await user.save();
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.status(200).json({ message: "User created successfully" });
+      const accessToken = jwt.sign(data, JWT_SERECT);
+      success = true;
+      res.json({ success, message: "User created successfully.", accessToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
     }
   }
 );
+
+// ROUTE 2 :  Authenticate a User using: POST "/api/auth/login". Doesn't require Auth
+router.post("/login", [body("identifier", "Enter a valid email or password.").exists(), body("password", "Password cannot be empty).").exists()], async (req, res) => {
+  // returning errors for bad requests
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  const { identifier, password } = req.body;
+  var success = false;
+  try {
+    var user = await User.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+    if (!user) {
+      success = false;
+      return res.status(400).json({ success, message: "Try Again!, Invalid User credentials." });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    // console.log(passwordCompare);
+    if (!passwordCompare) {
+      success = false;
+      return res.status(400).json({ success, message: "Try Again!, Invalid User credentials." });
+    }
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const accessToken = jwt.sign(data, JWT_SERECT);
+    success = true;
+    res.json({ success, message: "User Logged in successfully.", accessToken });
+  } catch (error) {
+    // console.error(error.message);
+    res.status(500).send("Internal Server Error.");
+  }
+});
 
 module.exports = router;
