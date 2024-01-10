1bbb1074c167234b8ab33bed6240981c07f4d516
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2024-01-10 23:35:36 +0545

    login page properly fixed and login funcationaility added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold">Blogs</h1>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
