7afae5ed029e00c38cb89c79bd65f11e21286c86
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2024-01-19 22:04:43 +0545

    responsive fixaed and implemented tinymce

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
     <>
       <BrowserRouter>
         <Navbar />
-        <div className="grid grid-cols-1 py-10 lg:grid-cols-3 w-2/3 gap-6 mx-auto">
-          <div className="mx-auto lg:col-span-2">
+        <div className="grid grid-cols-1 py-6 px-2 lg:grid-cols-3 lg:w-2/3 gap-6 mx-auto">
+          <div className="mx-auto w-full lg:col-span-2">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/login" element={<Login />} />
--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -1,15 +1,65 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import ThemeContext from "../context/ThemeContext";
+import { Editor } from "@tinymce/tinymce-react";
 
 const CreateBlog = () => {
   const { theme } = useContext(ThemeContext);
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
   return (
     <>
       <div className="mx-auto space-y-8">
         {/* card div */}
-        <div className={`p-4 w-full ${theme === "dark" ? "bg-[#344955]" : "bg-white"} rounded-md`}>
+        <div className={`py-4 px-2 w-full ${theme === "dark" ? "bg-[#344955]" : "bg-white"} rounded-md`}>
           <h3 className={`font-bold text-2xl inline-block ${theme === "dark" ? "text-white" : "text-[#344955]"}`}>Create a Blog</h3>
-          <form></form>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="editor">
+              <Editor
+                tinymceScriptSrc={"/tinymce/tinymce.min.js"}
+                onChange={(evt, editor) => (editorRef.current = editor)}
+                init={{
+                  height: 500,
+                  menubar: false,
+                  plugins: [
+                    "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "image",
+                    "charmap",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "preview",
+                    "help",
+                    "wordcount",
+                  ],
+                  toolbar:
+                    "undo redo | blocks | " +
+                    "bold italic forecolor | alignleft aligncenter " +
+                    "alignright alignjustify | bullist numlist outdent indent | ",
+                  content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                }}
+              />
+              <br />
+              <br />
+              <button onClick={log}>Log editor content</button>
+            </div>
+          </form>
         </div>
       </div>
     </>
