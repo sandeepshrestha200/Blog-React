9b38ce1c3a17e203046467376772cc841b886b48
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2024-02-02 22:53:49 +0545

    dotenv add and populate blogs on home aand Single blog redirect only

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,36 +1,37 @@
 import { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
-import imgi from "./assets/img/img.jpg";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 const BlogCard = (props) => {
   const { theme } = useContext(ThemeContext);
-  const { id } = props;
+  const { id, title, author, content, image, date } = props;
+  const history = useNavigate();
+
+  const imgUrl = import.meta.env.VITE_BACKEND_IMG;
+
+  const viewBlog = (id) => {
+    history(`/blog/${id}`);
+  };
 
   return (
     <div className="mx-auto">
       {/* card div */}
-      <div className={`p-4 w-full ${theme === "dark" ? "bg-[#344955]" : "bg-white"} rounded-md`}>
-        <h3 className="font-bold text-2xl hover:text-blue-600 cursor-pointer">{id} Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, ea.</h3>
+      <div
+        className={`p-4 w-full cursor-pointer ${theme === "dark" ? "bg-[#344955]" : "bg-white"} rounded-md`}
+        onClick={() => {
+          viewBlog(id);
+        }}
+      >
+        <h3 className="font-bold text-2xl hover:text-blue-600 cursor-pointer">{title}</h3>
         <p className="py-3">
-          by <span className="text-[#366bea] font-medium">Author</span> | timestamp
+          by <span className="text-[#366bea] font-medium">{author}</span> | {date}
         </p>
         <div className="sm:flex ">
-          <img src={imgi} alt="" className="w-[344px] h-[200px] sm:w-[145px] sm:h-[100px] rounded-md" />
-          <div className="px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos odio ab sapiente dolore, fugiat natus facere sequi praesentium minima vero.</div>
+          <img src={`${imgUrl}${image}`} alt={title} className="w-[344px] h-[200px] sm:w-[145px] sm:h-[100px] rounded-md" />
+          <div className="px-2">{content}</div>
         </div>
       </div>
-
-      {/* <div className={`p-4 w-full ${theme === "dark" ? "bg-[#344955]" : "bg-white"} rounded-md`}>
-        <h3 className="font-bold text-2xl hover:text-blue-600 cursor-pointer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, ea.</h3>
-        <p className="py-3">
-          by <span className="text-[#366bea] font-medium">Author</span> | timestamp
-        </p>
-        <div className="sm:flex ">
-          <img src={imgi} alt="" className="w-[344px] h-[200px] sm:w-[145px] sm:h-[100px] rounded-md" />
-          <div className="px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos odio ab sapiente dolore, fugiat natus facere sequi praesentium minima vero.</div>
-        </div>
-      </div> */}
     </div>
   );
 };
@@ -38,5 +39,10 @@ const BlogCard = (props) => {
 export default BlogCard;
 
 BlogCard.propTypes = {
-  id: PropTypes.number,
+  id: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  content: PropTypes.string,
+  image: PropTypes.string,
+  date: PropTypes.string,
 };
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 import SideBar from "./SideBar";
 
 const Home = () => {
-  const blogs = [{ _id: 1 }, { _id: 2 }, { _id: 3 }, { _id: 4 }];
+  const [blogs, setBlogs] = useState([]);
+  const backend_base = import.meta.env.VITE_BACKEND_BASE;
+
+
+  const fetchBlogs = async () => {
+    const response = await fetch(`${backend_base}/api/blogs/fetcharticles`, {
+      method: "GET",
+    });
+    const parseData = await response.json();
+    setBlogs(parseData);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <>
       <div className="grid grid-cols-1 py-6 px-2 lg:grid-cols-3 lg:w-2/3 gap-6 mx-auto">
         <div className={`mx-auto w-full lg:col-span-2`}>
           <div className="mx-auto space-y-8">
-            {blogs.map((e) => {
-              return <BlogCard key={e._id} id={e._id} />;
+            {blogs.map((blog) => {
+              return (
+                <>
+                  <BlogCard key={blog._id} id={blog._id} title={blog.title} image={blog.imageUrl} content={blog.content} author={blog.author_name} date={blog.dateTime} />
+                </>
+              );
             })}
           </div>
         </div>
